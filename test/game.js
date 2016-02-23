dd3d5bd0a919092159965a702ef97594454256e0
Alex Sell <alexanderjsell@gmail.com>
2016-02-23 19:01:21 +0000

    Roll Die function

--- test/game.js
+++ test/game.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect;
 var game = require('../lib/game');
 
+
 describe('rollDie()', function() {
     it('should roll a number between 1 and 6', function() {
         var n = game.rollDie();
