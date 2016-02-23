b5b5194b49f65e97c9c7f91abf18d8e734168f09
Alex Sell <alexanderjsell@gmail.com>
2016-02-23 20:02:14 +0000

    Score function and rollDice function

--- test/game.js
+++ test/game.js
@@ -9,3 +9,29 @@ describe('rollDie()', function() {
         expect(n).to.be.within(1,6);
     });
 });
+
+describe('rollDice(rolls)', function() {
+    it('should roll passed in number of dice', function() {
+        var array = game.rollDice(4);
+        
+        expect(array).to.have.length(4);
+    });
+});
+
+describe('score(rolls)', function() {
+    it('should calculate total score on turn', function() {
+        var total = game.score([4,3,4,2,5]);
+        
+       expect(total).to.equal(18);
+    });
+});
+
+describe('score(rolls)', function() {
+    it('should score 1 if 1 is rolled in rolls', function() {
+        var total = game.score([2,3,4,1]);
+        
+        expect(total).to.equal(1);
+    });
+});
+
+
